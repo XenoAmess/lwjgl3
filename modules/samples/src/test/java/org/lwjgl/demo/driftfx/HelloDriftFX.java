c667534efaa5d699d46b6f18d99c06c04065a00f
Ioannis Tsakpinis <iotsakp@gmail.com>
2019-09-18 19:40:03 +0300

    feat(driftfx): add lwjgl2 sync implementation

--- modules/samples/src/test/java/org/lwjgl/demo/driftfx/HelloDriftFX.java
+++ modules/samples/src/test/java/org/lwjgl/demo/driftfx/HelloDriftFX.java
@@ -167,6 +167,7 @@ public class HelloDriftFX extends Application {
                 glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
                 driftfxSurfacePresent(surface, target, DRIFTFX_CENTER);
+                //Sync.sync(60);
             }
         }
 
@@ -196,6 +197,136 @@ public class HelloDriftFX extends Application {
             }
         }
 
+    }
+
+    static final class Sync {
+
+        /** number of nano seconds in a second */
+        private static final long NANOS_IN_SECOND = 1000L * 1000L * 1000L;
+
+        /** The time to sleep/yield until the next frame */
+        private static long nextFrame = 0;
+
+        /** whether the initialisation code has run */
+        private static boolean initialised = false;
+
+        /** for calculating the averages the previous sleep/yield times are stored */
+        private static RunningAvg sleepDurations = new RunningAvg();
+        private static RunningAvg yieldDurations = new RunningAvg();
+
+        /**
+         * An accurate sync method that will attempt to run at a constant frame rate.
+         *
+         * <p>It should be called once every frame.</p>
+         *
+         * @param fps - the desired frame rate, in frames per second
+         */
+        static void sync(int fps) {
+            if (fps <= 0) {
+                return;
+            }
+            if (!initialised) {
+                initialise();
+            }
+
+            try {
+                // sleep until the average sleep time is greater than the time remaining till nextFrame
+                for (long t0 = System.nanoTime(), t1; (nextFrame - t0) > sleepDurations.avg(); t0 = t1) {
+                    Thread.sleep(1);
+                    sleepDurations.add((t1 = System.nanoTime()) - t0); // update average sleep time
+                }
+
+                // slowly dampen sleep average if too high to avoid yielding too much
+                sleepDurations.dampenForLowResTicker();
+
+                // yield until the average yield time is greater than the time remaining till nextFrame
+                for (long t0 = System.nanoTime(), t1; (nextFrame - t0) > yieldDurations.avg(); t0 = t1) {
+                    Thread.yield();
+                    yieldDurations.add((t1 = System.nanoTime()) - t0); // update average yield time
+                }
+            } catch (InterruptedException ignored) {
+            }
+
+            // schedule next frame, drop frame(s) if already too late for next frame
+            nextFrame = Math.max(nextFrame + NANOS_IN_SECOND / fps, System.nanoTime());
+        }
+
+        /**
+         * This method will initialise the sync method by setting initial
+         * values for sleepDurations/yieldDurations and nextFrame.
+         *
+         * If running on windows it will start the sleep timer fix.
+         */
+        private static void initialise() {
+            initialised = true;
+
+            sleepDurations.init(1000 * 1000);
+            yieldDurations.init((int)(-(System.nanoTime() - System.nanoTime()) * 1.333));
+
+            nextFrame = System.nanoTime();
+
+            String osName = System.getProperty("os.name");
+
+            if (osName.startsWith("Win")) {
+                // On windows the sleep functions can be highly inaccurate by
+                // over 10ms making in unusable. However it can be forced to
+                // be a bit more accurate by running a separate sleeping daemon
+                // thread.
+                Thread timerAccuracyThread = new Thread(() -> {
+                    try {
+                        Thread.sleep(Long.MAX_VALUE);
+                    } catch (Exception ignored) {
+                    }
+                });
+
+                timerAccuracyThread.setName("LWJGL Timer");
+                timerAccuracyThread.setDaemon(true);
+                timerAccuracyThread.start();
+            }
+        }
+
+        private static class RunningAvg {
+
+            private static final int SLOTS = 10;
+
+            private final long[] slots;
+            private       int    offset;
+
+            private static final long  DAMPEN_THRESHOLD = 10 * 1000L * 1000L; // 10ms
+            private static final float DAMPEN_FACTOR    = 0.9f; // don't change: 0.9f is exactly right!
+
+            RunningAvg() {
+                this.slots = new long[SLOTS];
+                this.offset = 0;
+            }
+
+            void init(long value) {
+                while (offset < SLOTS) {
+                    slots[offset++] = value;
+                }
+            }
+
+            void add(long value) {
+                slots[offset++ % SLOTS] = value;
+                offset %= SLOTS;
+            }
+
+            long avg() {
+                long sum = 0;
+                for (long slot : slots) {
+                    sum += slot;
+                }
+                return sum / SLOTS;
+            }
+
+            void dampenForLowResTicker() {
+                if (DAMPEN_THRESHOLD < avg()) {
+                    for (int i = 0; i < slots.length; i++) {
+                        slots[i] *= DAMPEN_FACTOR;
+                    }
+                }
+            }
+        }
     }
 
 }
